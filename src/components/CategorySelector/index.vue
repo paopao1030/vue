f602a08d60af8ace8b8daf50dfb3ea677d09bdb0
paopao <215866671@qq.com>
2020-05-21 22:10:39 +0800

    '5.21'

--- src/components/CategorySelector/index.vue
+++ src/components/CategorySelector/index.vue
@@ -1,105 +1,96 @@
 <template>
-  <!--查询表单-->
-  <el-form :inline="true" class="demo-form-inline">
-
-    <!-- 一级分类 -->
-    <el-form-item label="一级分类">
-      <el-select
-        v-model="category1Id"
-        placeholder="请选择"
-        @change="category1Changed">
-        <el-option
-          v-for="category in categoryList1"
-          :key="category.id"
-          :label="category.name"
-          :value="category.id"/>
-      </el-select>
-    </el-form-item>
-
-    <!-- 二级分类 -->
-    <el-form-item label="二级分类">
-      <el-select
-        v-model="category2Id"
-        placeholder="请选择"
-        @change="category2Changed">
-        <el-option
-          v-for="category in categoryList2"
-          :key="category.id"
-          :label="category.name"
-          :value="category.id"/>
-      </el-select>
-    </el-form-item>
-
-    <!-- 三级分类 -->
-    <el-form-item label="三级分类">
-      <el-select
-        v-model="category3Id"
-        placeholder="请选择"
-        @change="category3Changed">
-        <el-option
-          v-for="category in categoryList3"
-          :key="category.id"
-          :label="category.name"
-          :value="category.id"/>
-      </el-select>
-    </el-form-item>
-
-  </el-form>
-
+  <el-card class="box-card">
+    <el-form :inline="true" :model="cForm" class="demo-form-inline">
+      <el-form-item label="一级分类">
+        <el-select
+          v-model="cForm.category1Id"
+          placeholder="请选择一级分类"
+          @change="handleChange1"
+        >
+          <el-option
+            v-for="c in category1List"
+            :label="c.name"
+            :value="c.id"
+            :key="c.id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="二级分类">
+        <el-select
+          v-model="cForm.category2Id"
+          placeholder="请选择二级分类"
+          @change="handleChange2"
+        >
+          <el-option
+            :label="c2.name"
+            v-for="c2 in category2List"
+            :key="c2.id"
+            :value="c2.id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="三级分类">
+        <el-select v-model="cForm.category3Id" placeholder="请选择三级分类" @change="handleChange3">
+          <el-option
+            :label="c3.name"
+            :value="c3.id"
+            v-for="c3 in category3List"
+            :key="c3.id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
 
 <script>
 export default {
-
   data() {
     return {
-      // 查询表单数据
-      category1Id: null,
-      category2Id: null,
-      category3Id: null,
-      categoryList1: [],
-      categoryList2: [],
-      categoryList3: []
-    }
+      cForm: {
+        category1Id: "",
+        category2Id: "",
+        category3Id: ""
+      },
+      category1List: [], // 一级分类列表
+      category2List: [], // 二级分类列表
+      category3List: [] // 三级分类列表
+    };
   },
-
-  // 初始化一级类别
-  created() {
-    this.$API.category.getCategorys1().then(result => {
-      this.categoryList1 = result.data
-    })
+  mounted() {
+    this.getCategory1List();
   },
-
   methods: {
-    // 切换1级类别
-    category1Changed() {
-      this.categoryList2 = []
-      this.categoryList3 = []
-      this.category2Id = null
-      this.category3Id = null
-      this.$API.category.getCategorys2(this.category1Id).then(result => {
-        this.categoryList2 = result.data
-        this.$emit('listenOnSelect', this.category1Id, 1)
-      })
+    //一级下拉框的值发生变化,请求二级列表，并发送给父组件改变的数据
+    async handleChange1(category1Id) {
+      this.cForm.category2Id = "";
+      this.cForm.category3Id = "";
+      this.category2List = [];
+      this.category3List = [];
+      const result = await this.$API.category.getCategorys2(category1Id);
+      this.category2List = result.data;
+      this.$emit('categoryChange',{categoryId:category1Id,level:1})
     },
-
-    // 切换2级类别
-    category2Changed() {
-      this.categoryList3 = []
-      this.category3Id = null
-      this.$API.category.getCategorys3(this.category2Id).then(result => {
-        this.categoryList3 = result.data
-        this.$emit('listenOnSelect', this.category2Id, 2)
-      })
+    //二级下拉框的值发生变化
+    async handleChange2(category2Id) {
+      this.cForm.category3Id = "";
+      this.category3List = [];
+      const result = await this.$API.category.getCategorys3(category2Id);
+      this.category3List = result.data;
+      this.$emit('categoryChange',{categoryId:category2Id,level:2})
     },
-
-    // 显示属性列表
-    category3Changed() {
-      // 子组件向父组件传值
-      this.$emit('listenOnSelect', this.category3Id, 3)
+    //三级下拉框的值发生变化，并发送给父组件改变的数据
+    handleChange3(category3Id){
+      this.$emit('categoryChange',{categoryId:category3Id,level:3})
+    },
+    //获取一级列表
+    async getCategory1List() {
+      const result = await this.$API.category.getCategorys1();
+      this.category1List = result.data;
+    },
+    onSubmit() {
+      console.log("submit!");
     }
   }
-
-}
+};
 </script>
-
--- src/views/product/attr/list.vue
+++ src/views/product/attr/list.vue
@@ -1,13 +1,213 @@
 <template>
   <div>
-    平台属性管理
+    <CategorySelector @categoryChange="handleCategoryChange" />
+    <el-card class="box-card">
+      <div v-show="isShowList">
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          style="margin-bottom: 20px"
+          @click="showAdd"
+          >添加属性</el-button
+        >
+        <template>
+          <el-table :data="attrs" stripe style="width: 100%">
+            <el-table-column
+              label="序号"
+              type="index"
+              width="80"
+              align="center"
+            >
+            </el-table-column>
+            <el-table-column
+              label="属性名称"
+              width="180"
+              prop="attrName"
+            ></el-table-column>
+            <el-table-column label="属性值列表">
+              <template slot-scope="{ row, $index }">
+                <el-tag
+                  style="margin: 2px"
+                  type="info"
+                  v-for="value in row.attrValueList"
+                  :key="value.id"
+                  >{{ value.valueName }}</el-tag
+                >
+              </template>
+            </el-table-column>
+            <el-table-column prop="address" label="操作" width="150">
+              <template slot-scope="{ row }">
+                <HintButton
+                  title="修改"
+                  type="warn"
+                  icon="el-icon-edit"
+                  size="mini"
+                  @click="showUpdata(row)"
+                ></HintButton>
+                <HintButton
+                  title="删除"
+                  type="danger"
+                  icon="el-icon-delete"
+                  size="mini"
+                ></HintButton>
+              </template>
+            </el-table-column>
+          </el-table>
+        </template>
+      </div>
+      <div v-show="!isShowList">
+        <el-form inline :model="attr">
+          <el-form-item label="属性名">
+            <el-input
+              type="text"
+              v-model="attr.attrName"
+              placeholder="请输入属性名"
+            ></el-input>
+          </el-form-item>
+        </el-form>
+        <el-button type="primary" icon="el-icon-plus" @click="addAttrValue"
+          >添加属性值</el-button
+        >
+        <el-button @click="isShowList = true">取消</el-button>
+        <el-table border style="margin:20px 0" :data="attr.attrValueList">
+          <el-table-column label="序号" width="80" align="center" type="index">
+          </el-table-column>
+          <el-table-column label="属性值名称">
+            <template slot-scope="{ row, $index }">
+              <el-input
+                v-if="row.edit"
+                v-model="row.valueName"
+                size="mini"
+                placeholder="请输入属性值名称"
+                @blur="toShow(row)"
+                @keyup.enter.native="toShow(row)"
+              ></el-input>
+              <span v-else @click="toEdit(row)">{{ row.valueName }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作">
+            <template slot-scope="{ row, $index }">
+              <HintButton
+                title="删除"
+                type="danger"
+                icon="el-icon-delete"
+                size="mini"
+                @click="attr.attrValueList.splice($index, 1)"
+              ></HintButton>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-button type="primary">保存</el-button>
+        <el-button @click="isShowList = true">取消</el-button>
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AttrList',
-}
+  mounted() {
+    this.category1Id = 2;
+    this.category2Id = 13;
+    this.category3Id = 61;
+    this.getAttrs();
+  },
+  name: "AttrList",
+  methods: {
+    //查看模式改为编辑模式
+    toEdit(value) {
+      // 如果value就已经有edit属性了, 直接指为true
+      if (value.hasOwnProperty("edit")) {
+        value.edit = true;
+      } else {
+        // 如果没有, 必须通过$set()来添加一个新属性
+        this.$set(value, "edit", true);
+      }
+    },
+    //编辑模式失去焦点或者回车改为查看模式
+    toShow(value) {
+      if (value.valueName) {
+        const isReape = this.attr.attrValueList.some(item => {
+          if (item !== value) {
+            return item.valueName === value.valueName;
+          }
+        });
+        if (!isReape) {
+          value.edit = false;
+        } else {
+          value.valueName = "";
+          this.$message.warning("这个属性已经存在了!");
+        }
+      }
+    },
+    //点击添加属性的界面
+    showAdd() {
+      this.attr = {
+        // 要添加或者修改的平台属性对象
+        attrName: "", // 属性名
+        attrValueList: [], //属性值的列表
+        categoryId: this.category3Id, // 3级的分类ID
+        categoryLevel: 3 // 只能是3级
+      };
+      this.isShowList = false;
+    },
+    //修改属性值
+    showUpdata(attr) {
+      this.attr = attr;
+      this.isShowList = false;
+    },
+    //添加属性值
+    addAttrValue() {
+      this.attr.attrValueList.push({
+        valueName: "",
+        edit: true
+      });
+    },
+    //子组件三个级别发生变化传回来的参数
+    handleCategoryChange({ categoryId, level }) {
+      if (level === 1) {
+        //发送一级信息时，重置二级三级，并把展示数组改为【】
+        this.category1Id = categoryId;
+        this.category2Id = "";
+        this.category3Id = "";
+        this.attrs = [];
+      } else if (level === 2) {
+        this.category2Id = categoryId;
+        this.category3Id = "";
+        this.attrs = [];
+      } else {
+        this.category3Id = categoryId;
+        // 异步请求获取属性列表显示
+        this.getAttrs();
+      }
+    },
+    async getAttrs() {
+      const { category1Id, category2Id, category3Id } = this;
+      const result = await this.$API.attr.getList(
+        category1Id,
+        category2Id,
+        category3Id
+      );
+      this.attrs = result.data;
+    }
+  },
+  data() {
+    return {
+      category1Id: "",
+      category2Id: "",
+      category3Id: "",
+      attrs: [],
+      isShowList: true, //true为列表页面，false为添加修改页面
+      attr: {
+        // 要添加或者修改的平台属性对象
+        attrName: "", // 属性名
+        attrValueList: [], //属性值的列表
+        categoryId: "", // 3级的分类ID
+        categoryLevel: 3 // 只能是3级
+      }
+    };
+  }
+};
 </script>
 
 <style scoped>
@@ -20,4 +220,3 @@ export default {
   top: 10px;
 }
 </style>
-
